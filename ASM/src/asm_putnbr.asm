0a8bbfab2ed0e0b4368ba6f4bd027da36f85ee85
DarkyZShadow <s.soulie@hotmail.fr>
2017-03-22 14:18:47 +0100

    Added hex support for asm_putnbr

--- ASM/src/asm_putnbr.asm
+++ ASM/src/asm_putnbr.asm
@@ -29,13 +29,18 @@ _start:
     MOV RAX, RDI
     MOV RBX, RSI
 
-_loop:
+_loop_begin:
     XOR RDX, RDX            ; Clear high bits of RDX (dividend)
     IDIV RBX                ; Divide by 10
     PUSH RDX                ; Save remainder
     ADD BYTE [RSP], 0x30    ; Convert to printable char
+    CMP BYTE [RSP], 0x39    ; Is lower than '9' char
+    JLE _loop_end
+    ADD BYTE [RSP], 0x27    ; Convert 10 to 'a'
+
+_loop_end:
     CMP RAX, 0
-    JNE _loop
+    JNE _loop_begin
 
 _loop_print:                ; Basic print loop
     POP RDI
